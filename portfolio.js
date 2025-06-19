f5b300b12156634f1e2f35245fdb03dfd3fd7d9d
Victor Idowu <victoroluwapelumi504@gmail.com>
2025-06-20 00:46:06 +0200

    commit

---
+++ portfolio.js
@@ -0,0 +1,52 @@
+// window.addEventListener("DOMContentLoaded", () => {
+
+// const ctx = document.getElementById("skillChart").getContext("2d");
+// new Chart(ctx, {
+//   type: "bar",
+//   data: {
+//     labels: ["Frontend", "Backend", "Databases", "Cloud", "DevOps"],
+//     datasets: [
+//       {
+//         label: "Proficiency",
+//         data: [90, 80, 75, 60, 65],
+//         backgroundColor: "#ff5722",
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//         max: 100,
+//       },
+//     },
+//   },
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const ctx = document.getElementById("skillChart").getContext("2d");
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Frontend", "Backend", "Databases", "Cloud", "DevOps"],
+      datasets: [
+        {
+          label: "Proficiency",
+          data: [90, 80, 75, 60, 65],
+          backgroundColor: "#ff5722",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: 100,
+        },
+      },
+    },
+  });
+});
