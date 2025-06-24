1959c5e85a18b9a31e45c45dad65cebf86a6e757
Victor Idowu <victoroluwapelumi504@gmail.com>
2025-06-24 20:49:22 +0200

    Final

--- portfolio.js
+++ portfolio.js
@@ -25,3 +25,50 @@ window.addEventListener("DOMContentLoaded", () => {
     },
   });
 });
+
+function openModal(id) {
+  document.getElementById("modal-" + id).style.display = "flex";
+}
+
+function closeModal(id) {
+  document.getElementById("modal-" + id).style.display = "none";
+}
+
+function outsideClick(event, id) {
+  if (event.target.classList.contains("modal")) {
+    closeModal(id);
+  }
+}
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the normal form submission
+
+  const form = this;
+  const formData = new FormData(form);
+
+  fetch(form.action, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        // If email sent successfully, redirect to Thank You page
+        window.location.href = "thankyou.html";
+      } else {
+        // Show error message if there's a problem
+        response.json().then(function (data) {
+          if (data.errors) {
+            alert(data.errors.map((error) => error.message).join(", "));
+          } else {
+            alert("Oops! There was a problem submitting your form.");
+          }
+        });
+      }
+    })
+    .catch(function (error) {
+      alert("Oops! There was a problem submitting your form.");
+    });
+});
